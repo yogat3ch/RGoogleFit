dac79e41aecaadb0fa039b7dddf8870bf3d3510b
Marcin Szymanski <ms32035@gmail.com>
2016-04-26 20:31:04 +0000

    CRAN 0.1.0

--- RGoogleFit/R/RGoogleFit-package.R
+++ RGoogleFit/R/RGoogleFit-package.R
@@ -2,7 +2,7 @@
 #'
 #' @description
 #' Provides interface to Google Fit REST API v1
-#' (see https://developers.google.com/fit/rest/v1/reference/).
+#' (see <https://developers.google.com/fit/rest/v1/reference/>).
 #'
 #' Sources available at https://github.com/ms32035/RGoogleFit
 #'
